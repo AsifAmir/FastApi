2a3bda4dcfb7e5c468478bd2e9990cb98541147e
AsifAmir <amirasif411@gmail.com>
2026-04-14 05:02:39 +0600

    assign Id for posts

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from typing import Optional
-
+from random import randrange
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-
+posts = []
 class Post(BaseModel):
     Course_Code: str
     Course_Title: Optional[str] = None
     Course_Credits: int
-    Enrolled: bool = True
+    Enrolled: bool = False
 
 # decorator -> @app.get("/") -> HTTP method + path
 # path operation function -> root() -> function that is executed when the path is accessed
@@ -24,30 +24,16 @@ def root():
 def login():
     return {"message": "Login Page"}
 
-@app.get("/posts")
-def get_posts():
-    return {"data": "This is your posts"}
-
-# @app.post("/createposts")
-# def create_posts(payload: dict = Body(...)):
-#     print(payload)
-#     print(f'Course Code: {payload["Course_Code"]} Course Title: {payload["Course_Title"]}')
-#     print(f'Course Credits: {payload["Course_Credits"]}')
-
-#     return{"message": "Post created successfully", 
-#            "new_post": f"Course Code: {payload['Course_Code']}, Course Title: {payload['Course_Title']}, Course Credits: {payload['Course_Credits']}"}
 
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(payload: Post): # Using Pydantic model to validate the incoming data
-    print(payload)
+    # print(payload)
     payload_dict = payload.dict() # Convert the Pydantic model to a dictionary
+    payload_dict['id'] = randrange(0, 1000000) # Add a random id to the payload dictionary
     print(payload_dict)
-
-    # print(f'Course Code: {payload.Course_Code}, Course Title: {payload.Course_Title}, Course Credits: {payload.Course_Credits}')
-    # print(f'Enrolled: {payload.Enrolled}')
-    # return {"data": "This is your posts"}
-    # return{f'Course Code: {payload.Course_Code}, Course Title: {payload.Course_Title}, Course Credits: {payload.Course_Credits}, Enrolled: {payload.Enrolled}'}
-    # return{f'Enrolled: {payload.Enrolled}'}
-    # return{f'Course Title: {payload.Course_Title}'}
-    return{"data": payload_dict}
+    posts.append(payload_dict) # Add the new post to the list of posts
+    print(posts)
+    # return{"message": "Post created successfully","data": payload_dict,
+    #        "post": f"Course Code: {payload_dict['Course_Code']}, Course Title: {payload_dict['Course_Title']}, Course Credits: {payload_dict['Course_Credits']}, Enrolled: {payload_dict['Enrolled']}"}
+    return {"message": "Post created successfully", "data": payload_dict}
