7908d14f6ffbf455032020c78c669d754bdad289
AsifAmir <amirasif411@gmail.com>
2026-04-14 01:25:01 +0600

    post & get method

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
+
+# decorator -> @app.get("/") -> HTTP method + path
+# path operation function -> root() -> function that is executed when the path is accessed
 
 # 1. The Decorator
 @app.get("/")
@@ -8,8 +11,19 @@ app = FastAPI()
 def root():
     return {"message": "Hello World"}
 
-# 1. The Decorator
 @app.get("/login")
-# 2. The Path Operation Function
 def login():
     return {"message": "Login Page"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    print(f'Course Code: {payload["Course Code"]} Course Title: {payload["Course Title"]}')
+    print(f'Course Credits: {payload["Course Credits"]}')
+
+    return{"message": "Post created successfully", 
+           "new_post": f"Course Code: {payload['Course Code']}, Course Title: {payload['Course Title']}, Course Credits: {payload['Course Credits']}"}
